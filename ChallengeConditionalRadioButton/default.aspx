8f599318b627065b2c6f9bb37e8d326dfdcfabf2
ambateman <tmbateman@hotmail.com>
2017-09-30 15:15:03 -0700

    This if the CS-ASP0 012 Challenge involving radio buttons and images

---
+++ ChallengeConditionalRadioButton/default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengeConditionalRadioButton.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your Note Taking Preferences<br />
+            <br />
+            <asp:RadioButton ID="radBtnPencil" runat="server" GroupName="radDevices" OnCheckedChanged="RadioButton1_CheckedChanged" Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="radBtnPen" runat="server" GroupName="radDevices" OnCheckedChanged="RadioButton2_CheckedChanged1" Text="Pen" />
+            <br />
+            <asp:RadioButton ID="radBtnPhone" runat="server" GroupName="radDevices" Text="Phone" />
+        </div>
+        <asp:RadioButton ID="radBtnTablet" runat="server" GroupName="radDevices" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="btnOK" runat="server" OnClick="Button1_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="lblResult" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Image ID="imgDevice" runat="server" />
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton2_CheckedChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (radBtnPen.Checked)
+            {
+                lblResult.Text = "You like to use pens.";
+                imgDevice.ImageUrl = "pen.png";
+            }
+            else if (radBtnPencil.Checked) {
+                lblResult.Text = "You like to use pencils.";
+                imgDevice.ImageUrl = "pencil.png";
+
+            }
+            else if (radBtnPhone.Checked) {
+                lblResult.Text = "You like to use phones.";
+                imgDevice.ImageUrl = "phone.png";
+            }
+            else if (radBtnTablet.Checked){
+                lblResult.Text = "You like to use tablets.";
+                imgDevice.ImageUrl = "tablet.png";
+            }
+            else {
+                lblResult.Text = "Please select your favorite tool for taking notes.";
+            }
+
+
+        }   
+           
+
+
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
